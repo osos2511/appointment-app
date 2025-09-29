ee0c1d1a3bd1c3b2e18ba5a6bb60af9b18c77ace
osama mohamed <om329211@gmail.com>
2025-09-29 11:52:44 +0300

    +fake update

--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -7,6 +7,7 @@ class OnboardingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(child: Text('onboarding screen')),
+
     );
   }
 }
